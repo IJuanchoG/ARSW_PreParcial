8318ff26548a48cf2a91222fdc61a70221e8619d
JuanchoG <juan.garcia-ga@mail.escuelaing.edu.co>
2020-09-08 21:29:10 -0500

    punto 1 y 2

--- moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/MoneyLaunderingController.java
+++ moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/MoneyLaunderingController.java
@@ -18,5 +18,4 @@ public class MoneyLaunderingController
         return moneyLaunderingService.getSuspectAccounts();
     }
 
-    //TODO
 }
--- moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/service/MoneyLaunderingServiceStub.java
+++ moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/service/MoneyLaunderingServiceStub.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class MoneyLaunderingServiceStub implements MoneyLaunderingService {
     @Override
     public void updateAccountStatus(SuspectAccount suspectAccount) {
-        //TODO
+
     }
 
     @Override
